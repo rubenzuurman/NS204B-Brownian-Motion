9c170a197d63101626764bafb0a38b524b20bf32
unknown <rubenzuurman@gmail.com>
2024-09-14 21:03:28 +0200

    Implement video loading using cv2

--- __main__.py
+++ __main__.py
@@ -1,8 +1,7 @@
 from src.analysis import load_data
 
 def main():
-    data = ("data/test_13_09_2024_06_good.wmv")
-    print("Hello world!")
+    data = load_data("data/test_13_09_2024_06_good.wmv")
 
 if __name__ == "__main__":
     main()
--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,41 @@
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import pims
+
 def load_data(path: str):
-    pass
+    # Check if path exists.
+    assert os.path.isfile(path), f"File could not be found: '{path}'."
+    
+    # Check if filetype is valid.
+    assert os.path.splitext(path)[1] == ".wmv", "Only WMV files are supported currently."
+    
+    # Load video handle using cv2.
+    video_handle = cv2.VideoCapture(path)
+    
+    # Extract frames from video handle.
+    frames = []
+    while True:
+        # Do something?
+        video_handle.grab()
+        
+        # Get frame, it's a 3d np array with u8 pixel values (rgb).
+        return_value, image = video_handle.retrieve()
+        if not return_value:
+            break
+        
+        # Convert frame to grayscale (e.g. get green channel).
+        image_grayscale = image[:, :, 1]
+        
+        # Add frame to frames list.
+        frames.append(image_grayscale)
+    
+    # Save image (uncomment line below to save last frame in grayscale as test.png).
+    #plt.imsave("test.png", image_grayscale, cmap="gray", vmin=0, vmax=255)
+    
+    # Return list of frames.
+    return frames
 
 def preprocess(data):
     pass
