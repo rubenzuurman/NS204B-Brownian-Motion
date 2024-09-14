9097d6886b575541065338e7e4aa693e24963ba6
unknown <rubenzuurman@gmail.com>
2024-09-14 20:26:52 +0200

    Add data, add requirements, add elementary code setup

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from src.analysis import load_data
+
+def main():
+    data = ("data/test_13_09_2024_06_good.wmv")
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
---
+++ src/analysis.py
@@ -0,0 +1,8 @@
+def load_data(path: str):
+    pass
+
+def preprocess(data):
+    pass
+
+def get_trajectories(data):
+    pass
