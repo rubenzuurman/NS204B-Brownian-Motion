440a68f6b0dbfcfeb1bb7e9bcb307b675bc2a97e
unknown <rubenzuurman@gmail.com>
2024-09-24 12:33:44 +0200

    Add descriptive text to tune image

--- src/tune_parameters.py
+++ src/tune_parameters.py
@@ -18,12 +18,13 @@ def show_tune(frames, video_filename, particle_diameter, minimum_mass):
     batch_data = tp.batch(useful_frames, diameter=particle_diameter, invert=True, minmass=minimum_mass, processes=1)
     
     # Initialize plot.
-    fig, axs = plt.subplots(ncols=5, nrows=3, figsize=(24, 16), gridspec_kw={"height_ratios": [2, 1, 1]})
+    fig, axs = plt.subplots(ncols=5, nrows=3, figsize=(24, 16), gridspec_kw={"height_ratios": [1, 1, 1]})
     
     # Annotate each frame into plot.
     for index in range(len(frame_numbers)):
         # Plot annotated frames on row 0.
         tp.annotate(batch_data[batch_data["frame"] == index], useful_frames[index], ax=axs[0][index])
+        axs[0][index].set_title(f"Frame {frame_numbers[index]}")
         
         # Plot pixel biases on row 1.
         pixel_biases = batch_data[batch_data["frame"] == index][["x", "y"]].map(lambda x: x % 1)
@@ -38,6 +39,9 @@ def show_tune(frames, video_filename, particle_diameter, minimum_mass):
         axs[2][index].hist(batch_data[batch_data["frame"] == index]["mass"], bins=20)
         axs[2][index].set(xlabel="Mass", ylabel="Count")
     
+    mass_percent = int(video_filename[:3]) / 10
+    mass_percent, _, _, particle_size, magnification, sample_id, recording_id = video_filename[:-4].split("_")
+    fig.suptitle(f"{video_filename}\n{particle_size} at {magnification}\nSample {sample_id}{recording_id}", fontsize=20)
     fig.tight_layout()
     fig.savefig("tune_image.png")
     logger.info("Saved image to help tune parameters as 'tune_image.png'.")
